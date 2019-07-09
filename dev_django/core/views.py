4b74e08ddeadec4f500b1705ff54635036f7478d
John Cledson <cledsonjohn123@gmail.com>
2019-07-09 12:38:48 -0300

    Adicionando funcao soma

--- dev_django/core/views.py
+++ dev_django/core/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def hello(request, nome, idade):
     return HttpResponse(f'Hello {nome}, sua idade é {idade}')
+def soma(request, num1, num2):
+    return HttpResponse(f'A soma de {num1} e {num2} é {num1+num2}')
